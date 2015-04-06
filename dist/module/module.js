66fabbb9877a87d309f8e3e3d365dc612237d092
Mike Ryan <mike.ryan52@gmail.com>
2015-04-06 10:23:28 -0500

    Wrapped Module constructor in a factory function to expose easy chainability

--- dist/module/module.js
+++ dist/module/module.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _bind = Function.prototype.bind;
+
 var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } };
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
@@ -9,22 +11,22 @@ Object.defineProperty(exports, '__esModule', {
 });
 var _parsers = {};
 
-var Module = (function () {
-	function Module(name) {
+var DecoratedModule = (function () {
+	function DecoratedModule(name) {
 		var modules = arguments[1] === undefined ? false : arguments[1];
 
-		_classCallCheck(this, Module);
+		_classCallCheck(this, DecoratedModule);
 
 		this.name = name;
 
 		if (modules) {
-			this._module = angular.module(name, modules);
+			this._module = angular.module(name, Module.moduleList(modules));
 		} else {
 			this._module = angular.module(name);
 		}
 	}
 
-	_createClass(Module, [{
+	_createClass(DecoratedModule, [{
 		key: 'add',
 		value: function add() {
 			for (var _len = arguments.length, providers = Array(_len), _key = 0; _key < _len; _key++) {
@@ -47,38 +49,43 @@ var Module = (function () {
 		value: function publish() {
 			return this._module;
 		}
-	}], [{
-		key: 'moduleList',
-		value: function moduleList(modules) {
-			var realModuleList = [];
-
-			if (modules) {
-				for (var i = 0; i < modules.length; i++) {
-					if (modules[i].name) {
-						realModuleList.push(modules[i].name);
-					} else if (typeof modules[i] == 'string') {
-						realModuleList.push(modules[i]);
-					} else {
-						throw new Error('Cannot create submodule: unknown module type');
-					}
-				}
-			}
-
-			return realModuleList;
-		}
-	}, {
-		key: 'registerProvider',
-		value: function registerProvider(providerType, parser) {
-			_parsers[providerType] = parser;
-		}
-	}, {
-		key: 'getParser',
-		value: function getParser(providerType) {
-			return _parsers[providerType];
-		}
 	}]);
 
-	return Module;
+	return DecoratedModule;
 })();
+
+function Module() {
+	for (var _len2 = arguments.length, params = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+		params[_key2] = arguments[_key2];
+	}
+
+	return new (_bind.apply(DecoratedModule, [null].concat(params)))();
+}
+
+Module.moduleList = function (modules) {
+	var realModuleList = [];
+
+	if (modules) {
+		for (var i = 0; i < modules.length; i++) {
+			if (modules[i].name) {
+				realModuleList.push(modules[i].name);
+			} else if (typeof modules[i] === 'string') {
+				realModuleList.push(modules[i]);
+			} else {
+				throw new Error('Cannot create submodule: unknown module type');
+			}
+		}
+	}
+
+	return realModuleList;
+};
+
+Module.registerProvider = function (providerType, parser) {
+	_parsers[providerType] = parser;
+};
+
+Module.getParser = function (providerType) {
+	return _parsers[providerType];
+};
 
 exports.Module = Module;
--- src/module/module.js
+++ src/module/module.js
@@ -1,12 +1,12 @@
 let _parsers = {};
 
-export class Module{
+class DecoratedModule{
 	constructor(name, modules = false){
 		this.name = name;
 
 		if(modules)
 		{
-			this._module = angular.module(name, modules);
+			this._module = angular.module(name, Module.moduleList(modules));
 		}
 		else
 		{
@@ -32,36 +32,42 @@ export class Module{
 	publish(){
 		return this._module;
 	}
+}
 
-	static moduleList(modules){
-		let realModuleList = [];
+function Module(...params){
+	return new DecoratedModule(...params);
+}
 
-		if(modules){
-			for(let i = 0; i < modules.length; i++){
-				if(modules[i].name)
-				{
-					realModuleList.push(modules[i].name);
-				}
-				else if(typeof modules[i] == 'string')
-				{
-					realModuleList.push(modules[i]);
-				}
-				else
-				{
-					throw new Error('Cannot create submodule: unknown module type');
-				}
+Module.moduleList = function(modules){
+	let realModuleList = [];
+
+	if(modules){
+		for(let i = 0; i < modules.length; i++){
+			if(modules[i].name)
+			{
+				realModuleList.push(modules[i].name);
+			}
+			else if(typeof modules[i] === 'string')
+			{
+				realModuleList.push(modules[i]);
+			}
+			else
+			{
+				throw new Error('Cannot create submodule: unknown module type');
 			}
 		}
-
-		return realModuleList;
 	}
 
-	static registerProvider(providerType, parser){
-		_parsers[providerType] = parser;
-	
-	}
-
-	static getParser(providerType){
-		return _parsers[providerType];
-	}
+	return realModuleList;
 }
+
+Module.registerProvider = function(providerType, parser){
+	_parsers[providerType] = parser;
+
+}
+
+Module.getParser = function(providerType){
+	return _parsers[providerType];
+}
+
+export {Module};
