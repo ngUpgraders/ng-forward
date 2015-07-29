c9b29d2a9aca17cd6382892821e414e00a31ce66
Mike Ryan <mike.ryan52@gmail.com>
2015-07-28 19:53:53 -0500

    Adding experimental zone.js support

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,9 +1,30 @@
-/*global angular,document */
+/*global angular,document,global,zone */
 import bundle from './bundle';
 import {appWriter} from './writers';
 
+
+
 export default function bootstrap(component, otherProviders = []){
+  let ngZone;
   let selector = appWriter.get('selector', component);
+  let rootElement = document.querySelector(selector);
   bundle(selector, component, otherProviders);
-  angular.bootstrap(document.querySelector(selector), [selector]);
+
+  if(global.zone){
+    ngZone = zone.fork({
+      afterTask: () => {
+        let $rootScope = angular.element(rootElement).scope();
+        if($rootScope && !$rootScope.$$phase){
+          $rootScope.$digest();
+        }
+      }
+    });
+  }
+
+  if(ngZone){
+    ngZone.run(() => angular.bootstrap(rootElement, [selector]));
+  }
+  else{
+    angular.bootstrap(rootElement, [selector]);
+  }
 }
