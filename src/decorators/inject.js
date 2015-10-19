7b778306806c6ee6886e300adb261eaed87b1400
Tim Kindberg <timkindberg@gmail.com>
2015-10-19 09:51:21 -0400

    fix(Inject): better invalid provider error

--- src/decorators/inject.js
+++ src/decorators/inject.js
@@ -13,18 +13,31 @@
 // ```
 //
 // Import the appWriter for reading and writing metadata
-import {appWriter} from '../writers';
+import {appWriter, providerWriter} from '../writers';
 import {getInjectableName} from '../util/get-injectable-name';
 import {Providers} from '../decorators/Providers';
 
 // ## @Inject
 // Takes an array of injects
 export const Inject = ( ...injects ) => t => {
+	const ensureInjectable = inj => {
+		if (!providerWriter.get('name', inj) || !providerWriter.get('type', inj)) {
+			throw new Error(`Processing "${t.name}" @Inject parameter: "${inj.name || inj.toString()}" is not a valid injectable.
+			Please ensure ${inj.name || inj.toString()} is injectable. Valid examples can be:
+			 - a string representing an ng1 provider, e.g. '$q'
+			 - an @Injectable ng-forward class
+			 - a Provider, e.g. provide(SOME_CONFIG, {asValue: 100})`)
+		}
+		return inj;
+	};
 	// At the end of the day, Angular 1's DI requires the injection array to be
 	// an array of strings. Map over the injects to get the string provider name for
 	// each injectable
 
-	var providers = injects.filter(d => typeof d !== 'string');
+	var providers = injects
+			.filter(inj => typeof inj !== 'string')
+			.map(ensureInjectable);
+
 	Providers(...providers)(t);
 
 	let dependencies = injects.map(getInjectableName).filter(n => n !== undefined);
--- src/decorators/inject.spec.js
+++ src/decorators/inject.spec.js
@@ -1,7 +1,9 @@
 /* global describe, it */
 import {Inject} from './inject';
+import {Injectable} from './providers/injectable';
 import {appWriter} from '../writers';
 import chai from '../tests/frameworks';
+import {provide} from '../classes/provider';
 
 describe('@Inject annotation', function(){
 	it('should decorate a function with the $inject array', function(){
@@ -27,5 +29,22 @@ describe('@Inject annotation', function(){
 
 		appWriter.get('$inject', MyClass).should.eql(['a', 'b', 'c']);
 		appWriter.get('$inject', SubClass).should.eql(['d', 'e', 'f', 'a', 'b', 'c']);
+	});
+
+	it('should throw an error if injecting an invalid provider', function(){
+		@Injectable class ValidClass {}
+		let validNg1Service = '$q';
+		let validProvider = provide('foo', {useValue: 'foo'});
+		class NotValidClass {}
+
+		expect(() => {
+			@Inject(validNg1Service, ValidClass, validProvider)
+			class A{}
+		}).to.not.throw(Error);
+
+		expect(() => {
+			@Inject(validNg1Service, ValidClass, validProvider, NotValidClass)
+			class A{}
+		}).to.throw(/Processing "A" @Inject parameter: "NotValidClass" is not a valid injectable/);
 	});
 });
