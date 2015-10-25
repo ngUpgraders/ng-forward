fd831b2bd3448533efd607e5e5714c88eeb96921
Mike Ryan <mike.ryan52@gmail.com>
2015-10-25 12:50:51 -0500

    chore(build): Changing output to UMD and uglifying result

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import {rollup} from 'rollup';
 import babel from 'gulp-babel';
 import rename from 'gulp-rename';
 import rimraf from 'rimraf';
+import uglify from 'gulp-uglify';
 
 gulp.task('clean', done => {
 	rimraf('./dist', done);
@@ -13,7 +14,7 @@ gulp.task('transpile', () => {
 	let tsProject = ts.createProject('tsconfig.json');
 	let result = tsProject.src()
 		.pipe(ts(tsProject));
-		
+
 		return result.js.pipe(gulp.dest('dist'));
 });
 
@@ -21,7 +22,7 @@ gulp.task('bundle', ['transpile'], (async () => {
 	let bundle = await rollup({
 		entry: './dist/lib/index.js'
 	});
-	
+
 	await bundle.write({
 		format: 'cjs',
 		dest: './dist/ng-forward.es6.js'
@@ -30,7 +31,8 @@ gulp.task('bundle', ['transpile'], (async () => {
 
 gulp.task('build', ['bundle'], () => {
 	return gulp.src('./dist/ng-forward.es6.js')
-		.pipe(babel())
+		.pipe(babel({ modules: 'umd' }))
+		.pipe(uglify())
 		.pipe(rename('ng-forward.js'))
 		.pipe(gulp.dest('dist'));
 });
